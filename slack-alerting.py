bae1742445ab5245b1ec46aaa8eca6e627b31528
Jon Little <jon.little@ring.com>
2019-10-10 13:26:22 -0700

    added script
    this is a simple script please set ENV so script can work.

---
+++ slack-alerting.py
@@ -0,0 +1,32 @@
+import httplib, json
+import getopt, sys, os
+import subprocess
+
+
+def main():
+ 
+  headers = {'Content-Type': 'application/json'}
+  WEBHOOK_URL = os.environ['WEBHOOK_URL']
+  ICON_URL = os.environ['ICON_URL']
+  
+  color = 'good'
+  message = {
+    'username': 'Chef Guy',
+    'color': color,
+    'ICON_URL': ICON_URL,
+    'fields': [
+      {
+        'title': "AMI is ready",
+        'short': True
+      }
+    ]
+  }
+
+  connection = httplib.HTTPSConnection('hooks.slack.com')
+  connection.request('POST', WEBHOOK_URL, json.dumps(message), headers)
+  print json.dumps(message)
+  response = connection.getresponse()
+  print response.read().decode()
+
+if __name__ == '__main__':
+  main()
